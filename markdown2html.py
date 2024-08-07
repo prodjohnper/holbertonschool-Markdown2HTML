8fea34d9120ad2f2f455aef1a1abea9f2619f76f
prodjohnper <JonathanPerez9743@gmail.com>
2024-08-07 14:30:24 -0400

    Add - Created script that takes two args

--- markdown2html.py
+++ markdown2html.py
@@ -1,3 +1,35 @@
 #!/usr/bin/python3
+'''
+	0-Start a script
 
-# Write your code here :)
+	Script that converts a Markdown file to HTML
+'''
+
+
+import sys
+import os
+
+
+def main():
+    '''
+        Script that converts a Markdown file to HTML
+    '''
+    if len(sys.argv) < 3:
+
+        err = "Usage: ./markdown2html.py README.md README.html"
+
+        print(err, file=sys.stderr)
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.isfile(input_file):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
